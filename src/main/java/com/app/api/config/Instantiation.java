638672350074ad0f2531e3ddc1aef8dc2ae94e05
Bruno Santos <bruno_st17@hotmail.com>
2023-05-01 11:48:40 -0300

    Populando o banco para testes

---
+++ src/main/java/com/app/api/config/Instantiation.java
@@ -0,0 +1,24 @@
+package com.app.api.config;
+
+import com.app.api.entities.UserEntity;
+import com.app.api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        userRepository.deleteAll();
+        UserEntity personOne = new UserEntity(null, "Leticia", "leticia@gmail.com");
+        UserEntity personTwo = new UserEntity(null, "Marcos", "marcos@gmail.com");
+        UserEntity personThree = new UserEntity(null, "Kleber", "kleber@gmail.com");
+        userRepository.saveAll(Arrays.asList(personOne, personTwo, personThree));
+    }
+}
